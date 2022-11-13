d983943c344827ec3741bec45fc5aa81a65ec8be
Sara-M <sara.monintja@gmail.com>
2022-11-13 19:35:46 +0800

    added resolver

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,2 +1,27 @@
+const AuthenticationError = require('../exceptions/AuthenticationError');
 
+const { User } = require('../models');
+
+const { signToken } = require('../utils/auth');
+
+
+function checkIfLoggedIn(context){
+    if(!context.user){
+        throw new AuthenticationError('Not logged in');
+    }
+}
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            checkIfLoggedIn(context);
+            const userData = await User.findOne({ _id: context.user._id}).select('-__v -password');
+            return userData;
+        },
+    },
+
+    Mutation: {
+        
+    }
+}
 
